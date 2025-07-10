5273e3ac0450394ab116d4c2ce00f5734604ec8c
Angelo Panariti <angpanariti@gmail.com>
2025-07-10 18:09:34 +0200

    Minor changes

--- main.js
+++ main.js
@@ -4,53 +4,34 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
-
 renderer.setSize(window.innerWidth, window.innerHeight);
+// ClearColor
 renderer.setClearColor(0xcdf9ff);
 renderer.setPixelRatio(window.devicePixelRatio);
-
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(-10, 10, 10);
-
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.enablePan = false;
-controls.minDistance = 1;
-controls.maxDistance = 30;
-controls.minPolarAngle = 0.5;
-controls.maxPolarAngle = 1.5;
-controls.autoRotate = false;
-controls.target = new THREE.Vector3(0, 1, 0);
-controls.update();
 
+// Ground plane
 const groundGeometry = new THREE.CircleGeometry(8, 10 );
 groundGeometry.rotateX(-Math.PI / 2);
-const groundMaterial = new THREE.MeshBasicMaterial({
-    color: 0x738372,
-});
+const groundMaterial = new THREE.MeshBasicMaterial({ color: 0x738372, });
 const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
 groundMesh.castShadow = false;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-const spotLight = new THREE.DirectionalLight(0xffffff, 3000, 300, 0.22, 1);
-spotLight.position.set(0, 25, 0);
-spotLight.castShadow = true;
-spotLight.shadow.bias = -0.0001;
-scene.add(spotLight);
-
+// Load .gltf
 const loader = new GLTFLoader().setPath('model/');
 loader.load('scene.gltf', (gltf) => {
-    console.log('loading model');
+    console.log('Loading assets...');
     const mesh = gltf.scene;
-
     mesh.traverse((child) => {
         if (child.isMesh) {
             child.castShadow = true;
@@ -59,7 +40,7 @@ loader.load('scene.gltf', (gltf) => {
     });
 
     mesh.position.set(1, -0.37, 0.8);
-    mesh.scale.set(70, 70, 70);
+    mesh.scale.set(80, 80, 80);
     scene.add(mesh);
 
     document.getElementById('progress-container').style.display = 'none';
@@ -75,6 +56,18 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+// PerspectiveCamera --> OrbitControls Logic
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 1;
+controls.maxDistance = 30;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 1, 0);
+controls.update();
+
+// Animation, Scene Rendering...
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
